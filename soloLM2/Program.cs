514eaf1ac48e58fbaeb999b5ce668cef6032f568
SirTomax <46323295+SirTomax@users.noreply.github.com>
2019-01-03 09:28:14 +0100

    Add project files.

---
+++ soloLM2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace soloLM2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                // Get x
+                Console.Write("x = ");
+                string str = Console.ReadLine();
+                
+                // Exit?
+                if (str == "exit")
+                    break;
+
+                // Convert x
+                int x = Convert.ToInt32(str);
+                
+                // Get y
+                Console.Write("y = ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                
+                // calculate sum
+                int sum = x + y;
+                Console.WriteLine("Result: {0}", sum);
+            }
+            while (true); // repeat
+        }
+    }
+}
